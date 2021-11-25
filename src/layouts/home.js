da62767ee76d3db940a6ae3929caf0e52fe06be1
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 13:42:55 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -36,6 +36,7 @@ export default class Home extends React.Component {
                     <span class="more">Mouse over the card for more info</span>
                 </div>
             </div>
+        </
         <article key={index} className="post">
             
             <header className="post-header">
