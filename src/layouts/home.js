2a410b2f9967e12ee9a4b90a45c533331e7602b9
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 14:03:20 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -22,8 +22,7 @@ export default class Home extends React.Component {
             
         <article key={index} className="card">
 <aside class="profile-card">
-    <a target="_blank" href="#">
-      <img src="http://lorempixel.com/150/150/people/" class="hoverZoomLink"></a>
+    <a target="_blank" href="#"><img src="http://lorempixel.com/150/150/people/" class="hoverZoomLink"></a>
     <h1>John Doe</h1>
     <h2>Better Visuals</h2>
   <div class="profile-bio">
