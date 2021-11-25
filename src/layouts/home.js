8c81c746860560ce2068ac370ed2d78e2311022c
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 14:03:06 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -22,13 +22,11 @@ export default class Home extends React.Component {
             
         <article key={index} className="card">
 <aside class="profile-card">
-    <header>
     <a target="_blank" href="#">
       <img src="http://lorempixel.com/150/150/people/" class="hoverZoomLink">
     </a>
     <h1>John Doe</h1>
     <h2>Better Visuals</h2>
-    </header>
   <div class="profile-bio">
     <p>
       It takes monumental improvement for us to change how we live our lives. Design is the way we access that improvement.
