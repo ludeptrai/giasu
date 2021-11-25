86135c50f3a8a72bf41a77034c12053df81086d7
Stackbit Code Editor <projects@stackbit.com>
2021-11-25 17:09:20 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -17,7 +17,7 @@ export default class Home extends React.Component {
         const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
         const formattedDate = moment(date).strftime('%B %d, %Y');
 
-        const dayInMonth = moment(date).strftime('%B %d, %Y');
+        const dayInMonth = moment(date).strftime('%D');
         const month = moment(date).strftime('%M');
         const postUrl = getPageUrl(post, { withPrefix: true });
 
