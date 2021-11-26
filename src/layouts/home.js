25fe7c707c2f7ceb53a134a1509cc56f4f1611ac
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 02:40:04 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -13,7 +13,7 @@ export default class Home extends React.Component {
         const title = _.get(post, 'title');
         const thumbImage = _.get(post, 'thumb_img_path');
         const thumbImageAlt = _.get(post, 'thumb_img_alt', '');
-        const posted_by = _.get(post, 'posted_by','sdsdsd');
+        const postedBy = _.get(post, 'posted_by','sdsdsd');
         const date = _.get(post, 'date');
         const markdownContent = _.get(post, 'markdown_content');
         const dateTimeAttr = moment(date).strftime('%Y-%m-%d %H:%M');
