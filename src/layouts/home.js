009d19ddc511caf3c7869a08346cfd032b40baf7
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 03:00:51 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -45,7 +45,7 @@ export default class Home extends React.Component {
                     </div>
                         <p class="widget-49-meeting-item"><span>{htmlToReact(markdownContent)}</span></p>
                     <div class="widget-49-meeting-action">
-                        <a href="{url}" class="btn btn-sm btn-flash-border-primary">Chi tiết</a>
+                        <a href={url} class="btn btn-sm btn-flash-border-primary">Chi tiết</a>
                     </div>
                 </div>
             </div></div>
