9b11e0c9b2c2d922cef073cd57f66f9346261744
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 03:17:29 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -33,7 +33,7 @@ export default class Home extends React.Component {
            <div class="card">
             <div class="card-header no-border">
                 
-                <h5 class="card-title">{title}</h5>
+                <h5 class="card-title">{(title)}</h5>
             </div>
             <div class="card-body pt-0">
                 <div class="widget-49">
