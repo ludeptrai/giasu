26f4450d135c7d23daedca487db4a7650751eb90
Stackbit Code Editor <projects@stackbit.com>
2021-11-26 04:46:09 +0000

    home.js: changed

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -27,6 +27,8 @@ export default class Home extends React.Component {
         for (const tag in title) {
             text += '<p class="tag">' + title[tag] + '</p>'
         };
+
+        
         return (
             
         <article key={index} className="">
